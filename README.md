d6cb0e1d392f9707b9e3dec19393b8e15003f8fe
João Guilherme <joaog.alvesluiz@gmail.com>
2020-09-30 15:06:54 -0300

    Weather App using Geolocation with API

---
+++ README.md
@@ -0,0 +1,22 @@
+# Weather App
+## Simple system to show the Weather by Geolocation, using API.
+
+### With the following features
+
+* Current Weather Icon
+* Degrees of Temperature
+* Converter from ºC to ºF by clicking on temperature
+* Weather Description
+* City
+
+### 🛠 Technologies
+
+The following tools were used in the construction of the project:
+
+* Javascript
+* Geolocation (https://openweathermap.org/)
+
+### Pictures 
+  App View
+
+  <img alt="Weather App" title="#Weather App" src="./images/weather.app.png" />
---
+++ scripts/main.js
@@ -0,0 +1,104 @@
+// Tutorial by http://youtube.com/CodeExplained
+// api key : 4dcea32dc182951eb230c6a962d8024c
+
+//Selecting all elements
+const iconElement = document.querySelector(".weather-icon");
+const tempElement = document.querySelector(".temperature-value p");
+const descElement = document.querySelector(".temperature-description p");
+const locationElement = document.querySelector(".location p");
+const notificationElement = document.querySelector(".notification");
+
+
+//App Data
+const weather = {};
+
+weather.temperature = {
+    unit: "celsius"
+};
+
+// App Const and Vars
+const KELVIN = 273;
+
+//Api Key
+const key = "4dcea32dc182951eb230c6a962d8024c";
+
+//Check if Browser Support Geolocation
+if("geolocation" in navigator) {
+   
+    navigator.geolocation.getCurrentPosition(setPosition, showError);
+
+} else {
+    notificationElement.style.display = "block"
+    notificationElement.innerHTML = "<p>Browser doesn't Support Geolocation</p>"
+}
+
+// Set Users Position
+function setPosition(position) {
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+
+    getWeather(latitude, longitude);
+}
+
+//Show error when there is an issue with geolocation service
+function showError(error) {
+    notificationElement.style.display = "block"
+    notificationElement.innerHTML = `<p> ${error.mesage} </p>`;
+}
+
+//Get Weather from Api
+function getWeather(latitude, longitude) {
+
+    let test = `http://api.openweathermap.org/data/2.5/weather?id=524901&appid=${key}`;
+
+    let api =  `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+
+    fetch(api)
+        .then(function(response) {
+            let data = response.json();
+            return data;
+        })
+        .then(function(data) {
+            weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+            weather.description = data.weather[0].description;
+            weather.iconId = data.weather[0].icon;
+            weather.city = data.name;
+            weather.country = data.sys.country;
+        })
+        .then(function() {
+            displayWeather();
+        }) 
+
+} 
+
+//Show in Screen
+function displayWeather() {
+    iconElement.innerHTML = `<img src="images/${weather.iconId}.png" >`;
+    tempElement.innerHTML = `${weather.temperature.value}º <span>C</span>`;
+    descElement.innerHTML = weather.description;
+    locationElement.innerHTML = `${weather.city}, ${weather.country} `; 
+}
+
+//C to F Conversion
+function celsiusToFahrenheit(temperature) {
+    return (temperature * 9/5 ) + 32;
+}
+
+//Conversão na tela
+tempElement.addEventListener("click", () => {
+    if(weather.temperature.value === undefined) return;
+
+    if(weather.temperature.unit == "celsius") {
+
+        let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
+        fahrenheit = Math.floor(fahrenheit);
+        tempElement.innerHTML = `${fahrenheit}º <span>F</span>`;
+        weather.temperature.unit = "fahrenheit";
+
+    } else {
+
+        tempElement.innerHTML = `${weather.temperature.value}º <span>C</span>`;
+        weather.temperature.unit = "celsius";
+
+    }
+})
